8750f217283ba6c0936be9542593c4f85b148283
Akyrka <gyg283169@gmail.com>
2025-09-21 12:20:47 +0300

    Revert "login"

--- diary/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import user_login, register, user_logout
-from django.urls import path
-urlpatterns = [
-    path("login/", user_login, name="login"),
-    path("register/", register, name="register"),
-    path("logout/", user_logout, name="logout"),
-]
